82334d1e02a715c85177d133acf97fd9f00bb4dc
Hama Pashazadeh <pashazad@msu.edu>
2025-01-07 14:18:09 -0500

    Add logo placeholder and header title to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ export default function Header() {
     return (
         <>
             <header>
+                {/* Insert logo/ image  */}
                 <nav>
+                    <h1>Who Is Hama?</h1>
                     <a href="#">Early Life</a>
                     <a href="#">Computer Science</a>
                     <a href="#">Future Plans</a>
